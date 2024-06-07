92bd2e6eadc66af5e5f44b716b43c6c7d8c7010f
continuematical <zhou42538@outlook.com>
2024-06-07 16:15:19 +0800

    fix: :bug: fix bug

--- demo/src/main/java/com/example/demo/Controller/HelloController.java
+++ demo/src/main/java/com/example/demo/Controller/HelloController.java
@@ -3,6 +3,10 @@ package com.example.demo.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+//@ResponseBody
+//@Controller
+
 @RestController
 public class HelloController {
 
