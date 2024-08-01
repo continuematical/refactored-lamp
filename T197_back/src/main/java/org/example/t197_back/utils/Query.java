06da7a7481282e2079625fe1cbaacd9c4f270620
continuematical <zhou42538@outlook.com>
2024-08-01 15:02:42 +0800

    feat: :sparkles: 创建自定义查询类

--- T197_back/src/main/java/org/example/t197_back/utils/Query.java
+++ T197_back/src/main/java/org/example/t197_back/utils/Query.java
@@ -1,6 +1,7 @@
 package org.example.t197_back.utils;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.plugins.Page;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -25,8 +26,41 @@ public class Query<T> extends LinkedHashMap<String, Object> {
 
         //分页参数
         if (params.get("page") != null) {
-
+            currentPage = Integer.parseInt((String) params.get("page"));
+        }
+        if (params.get("limit") != null) {
+            limit = Integer.parseInt((String) params.get("limit"));
         }
 
+        this.put("offset", (currentPage - 1) * limit);
+        this.put("limit", limit);
+        this.put("page", currentPage);
+
+        //防止SQL注入
+        String sidx = SQLFilter.sqlInject((String)params.get("sidx"));
+        String order = SQLFilter.sqlInject((String)params.get("order"));
+        this.put("sidx", sidx);
+        this.put("order", order);
+
+        //mybatis-plus分页
+        this.page = new Page<>(currentPage, limit);
+
+        //排序
+        if(StringUtils.isNotBlank(sidx) && StringUtils.isNotBlank(order)){
+            this.page.setOrderByField(sidx);
+            this.page.setAsc("ASC".equalsIgnoreCase(order));
+        }
+    }
+
+    public Page<T> getPage() {
+        return page;
+    }
+
+    public int getCurrPage() {
+        return currentPage;
+    }
+
+    public int getLimit() {
+        return limit;
     }
 }
