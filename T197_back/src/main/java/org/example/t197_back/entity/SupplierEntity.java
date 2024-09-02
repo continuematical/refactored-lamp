3ee0cf5f452a1156f76d78a6caaa463cbc2cdaf3
continuematical <zhou42538@outlook.com>
2024-09-02 08:11:13 +0800

    feat: :sparkles: 所有更改

---
+++ T197_back/src/main/java/org/example/t197_back/entity/SupplierEntity.java
@@ -0,0 +1,2 @@
+package org.example.t197_back.entity;public class SupplierEntity {
+}
---
+++ T197_back/src/main/java/org/example/t197_back/utils/StringUtil.java
@@ -0,0 +1,11 @@
+package org.example.t197_back.utils;
+
+public class StringUtil {
+    public static boolean isEmpty(String str) {
+        return str == null || str.length() == 0;
+    }
+
+    public static boolean isNotEmpty(String str) {
+        return !isEmpty(str);
+    }
+}
